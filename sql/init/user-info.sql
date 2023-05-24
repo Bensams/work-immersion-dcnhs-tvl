068b1cce293606127e1b8bd5e41f72b99d582c13
Benedict Paul <98753187+Bensams@users.noreply.github.com>
2023-05-24 14:46:59 +0800

    LAST Update user-info.sql 5/24/2023

--- sql/init/user-info.sql
+++ sql/init/user-info.sql
@@ -1,6 +1,8 @@
 
 CREATE TABLE IF NOT EXISTS UserInfo (
 	id INT NOT NULL,
+	FirstName TEXT NOT NULL,
+	LastName TEXT NOT NULL,
 	userType TEXT NOT NULL,
 	userName TEXT NOT NULL,
 	password TEXT NOT NULL,
