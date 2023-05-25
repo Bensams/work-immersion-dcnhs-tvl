60d25d9b1ee500c2911f0e6f41bec9da1fdc8f2f
Benedict Paul <98753187+Bensams@users.noreply.github.com>
2023-05-25 13:51:45 +0800

    Change all identifiers to camel case

--- sql/init/room.sql
+++ sql/init/room.sql
@@ -1,10 +1,10 @@
-
 CREATE TABLE IF NOT EXISTS Room (
 	id INT NOT NULL,
 	-- TODO: implement supplemental data
-	RoomNumber INT NOT NULL,
-	RoomType TEXT NOT NULL,
-	Rate INT NOT NULL,
-	Capacity INT NOT NULL
+	roomNumber INT NOT NULL,
+	roomType TEXT NOT NULL,
+	rate INT NOT NULL,
+	capacity INT NOT NULL
+	
 	PRIMARY KEY (id)
 );
